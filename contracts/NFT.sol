a59c226a70a5a6847343899c3152bfdb03df9da1
SenudaJayalath <senudajayalath1@gmail.com>
2022-02-07 00:48:17 +0530

    Minting with linking metadata

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,17 +6,30 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721 {
     using Counters for Counters.Counter;
+
     Counters.Counter private currentTokenId;
 
-    constructor() ERC721("NFTTutorial", "NFT") {}
+    /// @dev Base token URI used as a prefix by tokenURI().
+    string public baseTokenURI;
 
-    function mintTo(address recipient)
-        public
-        returns (uint256)
-    {
+    constructor() ERC721("NFTTutorial", "NFT") {
+        baseTokenURI = "";
+    }
+
+    function mintTo(address recipient) public returns (uint256) {
         currentTokenId.increment();
         uint256 newItemId = currentTokenId.current();
         _safeMint(recipient, newItemId);
         return newItemId;
+    }
+
+    /// @dev Returns an URI for a given token ID
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    /// @dev Sets the base token URI prefix.
+    function setBaseTokenURI(string memory _baseTokenURI) public {
+        baseTokenURI = _baseTokenURI;
     }
 }

8b1aba97715c82d453216fcb97da9bf82e24b742
SenudaJayalath <senudajayalath1@gmail.com>
2022-02-09 10:31:18 +0530

    Modify smart contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,6 +7,10 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract NFT is ERC721 {
     using Counters for Counters.Counter;
 
+    // Constants
+    uint256 public constant TOTAL_SUPPLY = 10_000;
+    uint256 public constant MINT_PRICE = 0.0006 ether;
+
     Counters.Counter private currentTokenId;
 
     /// @dev Base token URI used as a prefix by tokenURI().
@@ -16,7 +20,14 @@ contract NFT is ERC721 {
         baseTokenURI = "";
     }
 
-    function mintTo(address recipient) public returns (uint256) {
+    function mintTo(address recipient) public payable returns (uint256) {
+        uint256 tokenId = currentTokenId.current();
+        require(tokenId < TOTAL_SUPPLY, "Max supply reached");
+        require(
+            msg.value == MINT_PRICE,
+            "Transaction value did not equal the mint price"
+        );
+
         currentTokenId.increment();
         uint256 newItemId = currentTokenId.current();
         _safeMint(recipient, newItemId);
